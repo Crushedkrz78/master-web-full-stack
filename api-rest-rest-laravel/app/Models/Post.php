3d0e6669b3fec0641d2194a9607f9f3bca9e2a6d
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2021-03-25 23:42:27 -0600

    14.[Laravel] Creando los modelos

---
+++ api-rest-rest-laravel/app/Models/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $table = 'posts';
+
+    //Relación de uno a muchos (Inversa)
+    public function user(){
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    public function category(){
+        return $this->belongsTo('App\Category', 'category_id');
+    }
+}
--- api-rest-rest-laravel/app/Models/User.php
+++ api-rest-rest-laravel/app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    //Relación de uno a muchos
+    public function posts(){
+        return $this->hasMany('App\Post');
+    }
 }
