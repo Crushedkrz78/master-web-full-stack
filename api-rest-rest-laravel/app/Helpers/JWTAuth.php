2af762238824847e8b79f8096e860a4c872374cd
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2021-04-27 23:57:56 -0500

    Prev Changes

--- api-rest-rest-laravel/app/Helpers/JWTAuth.php
+++ api-rest-rest-laravel/app/Helpers/JWTAuth.php
@@ -7,13 +7,55 @@ use App\User;
 
 class JwtAuth{
 
-    public function signup(){
-        // Buscar si existe el usuario con las credenciales
-        // Comprobar si las credenciales son correctas (Objeto)
-        // Generar Token con los datos del usuario identificado
-        // Devolver los datos decodificados o el Token, en función del un parámetro
+    public $key;
 
-        return 'Método de clase JWTAUTH';
+    public function __construct(){
+        $this->key = 'esto_es_una_clave_super_secreta'
+
+    }
+
+    public function signup($email, $password, $token = null){
+        // Buscar si existe el usuario con las credenciales
+        $user = User::where([
+            'email' => $email,
+            'password' => $password
+        ])->first();
+        // Comprobar si las credenciales son correctas (Objeto)
+        $signup = false;
+        if(is_object($user)){
+            $signup = true;
+        }
+        // Generar Token con los datos del usuario identificado
+        if($signup){
+            $token = array(
+                'sub' => $user->id,
+                'email' => $user->email,
+                'name'=> $user->name,
+                'surname'=> $user->surname,
+                'iat' => time(),
+                'exp' => time() + (7 * 24 * 60 * 60)
+            );
+
+            $jwt = JWT::encode($token, $this->key, 'HS256');
+            $decoded = JWT::decode($jwt, $this->key, ['HS256']);
+
+            // Devolver los datos decodificados o el Token, en función del un parámetro
+            if(is_null($getToken)){
+                $data = $jwt;
+            }else{
+                $data = $decoded;
+            }
+
+        }else{
+            $data = array(
+                'status'=> 'error',
+                'message'=> 'Login incorrecto'
+            );
+        }
+
+
+
+        return $data;
     }
 
 }
