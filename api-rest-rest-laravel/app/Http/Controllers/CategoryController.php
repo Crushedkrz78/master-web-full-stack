5bcca7a0d053c8d256da3bd459a2e943938bdfbf
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2021-10-26 23:35:31 -0500

    36.[Laravel] Obtener las categorías

--- api-rest-rest-laravel/app/Http/Controllers/CategoryController.php
+++ api-rest-rest-laravel/app/Http/Controllers/CategoryController.php
@@ -3,10 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    public function pruebas(Request $request){
-        return "Acción de pruebas de CATEGORY-CONTROLLER";
+    public function index(){
+        $categories = Category::all();
+
+        return response()->json([
+            'code' => 200,
+            'status' => 'success',
+            'categories' => $categories
+        ]);
+    }
+
+    public function show($id){
+        $category = Category::find($id);
+
+        if(is_object($category)){
+            $data = [
+                'code' => 200,
+                'status' => 'success',
+                'categories' => $category
+            ];
+        }else{
+            $data = [
+                'code' => 400,
+                'status' => 'error',
+                'categories' => 'La categoría no existe'
+            ];
+        }
+
+        return response()->json($data, $data['code']);
     }
 }
