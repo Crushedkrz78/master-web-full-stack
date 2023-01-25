7c3be398b16eae517a33110ba2d7c3ba2b64fe4b
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2023-01-25 14:29:18 -0600

    58. Creación de componentes en el proyecto

--- blog-angular/src/app/app.routing.ts
+++ blog-angular/src/app/app.routing.ts
@@ -4,13 +4,16 @@ import { Routes, RouterModule } from '@angular/router';
 // Import de componentes
 import { LoginComponent } from "./components/login/login.component";
 import { RegisterComponent } from "./components/register/register.component";
+import { HomeComponent } from "./components/home/home.component";
+import { ErrorComponent } from "./components/error/error.component";
 
 // Definicion de rutas
 const appRoutes: Routes = [
-  {path: '', component: LoginComponent},
+  {path: '', component: HomeComponent},
   {path: 'inicio', component: LoginComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: '**', component: ErrorComponent}
 ];
 
 // Export configuracion de rutas
