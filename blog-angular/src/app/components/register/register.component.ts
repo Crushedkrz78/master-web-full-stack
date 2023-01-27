32414320931d2d5480dba1effdb9acb63bb0d493
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2023-01-27 12:38:26 -0600

    63. Crear servicio de usuarios

--- blog-angular/src/app/components/register/register.component.ts
+++ blog-angular/src/app/components/register/register.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [UserService]
 })
 export class RegisterComponent implements OnInit {
 
   public page_title: string;
   public user: User;
 
-  constructor() {
+  constructor(
+    private _userService: UserService
+  ) {
     this.page_title = 'Registrate';
     this.user = new User(1, '', '', 'Role_user', '', '', '', '');
   }
@@ -22,7 +26,10 @@ export class RegisterComponent implements OnInit {
 
   onSubmit(form){
     console.log(this.user);
+    console.log(this._userService.test());
     form.reset();
   }
+
+
 
 }
